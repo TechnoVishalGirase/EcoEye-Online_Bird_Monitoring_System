265800763fa2a8372756e20fdf7674ce8ffa9829
TechnoVishalGirase <158005090+TechnoVishalGirase@users.noreply.github.com>
2024-04-08 14:38:45 -0400

    Add files via upload

---
+++ week_14/ecobird/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name ="index"),
+    path('error/',views.error_, name = 'error'),
+    path('about/',views.about, name = "about"),
+    path('booking/',views.booking, name = "booking"),
+    path('destination/',views.destination, name = "destination"),
+    path('service/',views.service, name = "service"),
+    path('package/',views.package, name = "package"),
+    path('team/',views.team, name = "team"),
+    path('testimonial/',views.testimonial, name = "testimonial"),
+    path('contact/',views.contact, name = "contact"),
+    path('predict/', views.predict, name="predict"),
+]
